b469a674c3ac032d35b3cc43b52e5d1282ca87e6
nikitazozoulenko <nikita.zozoulenko@gmail.com>
2018-01-14 23:57:02 +0100

    implemented neural style (style transfer)

---
+++ slow_style_transfer/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def show_image(var_image):
+    var_image = var_image*127.5 + 127.5
+    numpy_image = var_image.data[0].permute(1,2,0).cpu().numpy()
+
+    im = Image.fromarray(numpy_image.astype(np.uint8))
+    im.show()
+
+def graph_losses(losses):
+    plt.plot(losses, "b", label="Total Loss")
+    plt.legend(loc=1)
+    plt.show()
