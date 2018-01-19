306b261e19b2d1a8fb321f27579672b4fb71d83c
nikitazozoulenko <nikita.zozoulenko@gmail.com>
2018-01-19 22:24:17 +0100

    fixed show_image() function for new input range

--- slow/utils.py
+++ slow/utils.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 def show_image(var_image):
-    var_image = var_image
     numpy_image = var_image.data[0].permute(1,2,0).cpu().numpy()
 
     im = Image.fromarray(numpy_image.astype(np.uint8))
